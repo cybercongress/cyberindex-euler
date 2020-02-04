36600bcd145fd8bdebe69bd45465dcd880508da7
Anatoli Belchikov <belchikov1996@gmail.com>
2020-02-04 19:42:30 +0300

    Fixed rewards views

--- schema/rewards.sql
+++ schema/rewards.sql
@@ -1,26 +1,23 @@
+CREATE VIEW linkage_view AS (
+    SELECT cid, agent, MIN(height) AS height, MIN(timestamp) AS timestamp
+    FROM (
+        SELECT object_to AS cid, subject AS agent, MIN(height) AS height, MIN(timestamp) AS timestamp
+        FROM cyberlink
+        GROUP BY object_to, subject
+        
+        UNION
+        
+        SELECT object_from AS cid, subject AS agent, MIN(height) AS height, MIN(timestamp) AS timestamp
+        FROM cyberlink
+        GROUP BY object_from, subject
+    ) AS merged_linkage
+    GROUP BY cid, agent
+)
+
 CREATE VIEW rewards_view AS (
-    WITH linkage AS (
-        SELECT cid, agent, MIN(height) AS height, MIN(timestamp) AS timestamp
-        FROM (
-            SELECT object_to AS cid, subject AS agent, MIN(height) AS height, MIN(timestamp) AS timestamp
-            FROM cyberlink
-            GROUP BY object_to, subject
-            
-            UNION
-            
-            SELECT object_from AS cid, subject AS agent, MIN(height) AS height, MIN(timestamp) AS timestamp
-            FROM cyberlink
-            GROUP BY object_from, subject
-        ) AS merged_linkage
-        GROUP BY cid, agent
-    ), top_cids AS (
+    WITH top_cids AS (
         SELECT cid, block, rank
         FROM relevance
-    ), top_cids_sum AS (
-        SELECT block, sum(rank) AS rank_sum
-        FROM top_cids
-        WHERE cid IN (SELECT cid FROM linkage)
-        GROUP BY block
     ), B_linked_agents AS (
         SELECT * 
         FROM (
@@ -28,13 +25,9 @@ CREATE VIEW rewards_view AS (
               PARTITION BY cid
               ORDER BY timestamp
             ) order_number
-            FROM linkage
+            FROM linkage_view
         ) AS tmp
         WHERE order_number <= 10
-    ), B_linked_agents_max_order AS (
-        SELECT block, cid, max(order_number) AS max_order_number
-        FROM B_linked_agents
-        GROUP BY block, cid
     )
     SELECT 
         B_linked_agents.agent, 
@@ -42,15 +35,17 @@ CREATE VIEW rewards_view AS (
         top_cids.block AS block, 
         B_linked_agents.block AS test_block,
         top_cids.rank, 
-        B_linked_agents.order_number,
-        top_cids_sum.rank_sum,
-        B_linked_agents_max_order.max_order_number
-    FROM top_cids, B_linked_agents, top_cids_sum, B_linked_agents_max_order
+        B_linked_agents.order_number
+    FROM top_cids, B_linked_agents
     WHERE top_cids.cid = B_linked_agents.cid
     AND B_linked_agents.block <= top_cids.block
-    AND top_cids_sum.block = top_cids.block
-    AND (
-        B_linked_agents_max_order.cid = B_linked_agents.cid 
-        AND B_linked_agents_max_order.block = B_linked_agents.block
-    )
+)
+
+
+CREATE VIEW linkages_view AS (
+    SELECT linkage_view.cid, block, count(*) AS linkages
+    FROM linkage_view, relevance
+    WHERE linkage_view.cid = relevance.cid
+    AND linkage_view.height <= relevance.block
+    GROUP BY linkage_view.cid, block
 )
