8cff98f6940231fc6afe8ba001ed142016e7f12e
Ales Puchilo <ales.puchilo@gmail.com>
2020-03-22 14:09:09 +0300

    fix karma top view

--- schema/karma.sql
+++ schema/karma.sql
@@ -1,14 +1,4 @@
 CREATE VIEW karma_view AS (
-    SELECT subject, links_count.height, links * COALESCE(price, 0.01) * 100 AS karma
-    FROM (
-        SELECT subject, height, count(*) AS links
-        FROM cyberlink
-        GROUP BY subject, height
-    ) AS links_count 
-    LEFT JOIN (
-        SELECT height, price 
-        FROM bandwidth_price
-    ) AS bandwidth_price_tmp
-    ON
-        links_count.height = bandwidth_price_tmp.height
+    SELECT cyberlink.subject, SUM(cyberlink.karma) AS "karma" FROM cyberlink
+    GROUP BY cyberlink.subject
 )
