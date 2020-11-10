d26b68abbf45e77f40d7d85739be76ec3db61bc4
Ales Puchilo <ales.puchilo@gmail.com>
2020-11-10 20:24:39 +0300

    fix decorations in twitter analytics

--- schema/twitter.sql
+++ schema/twitter.sql
@@ -5,12 +5,13 @@ CREATE VIEW tweets_per_day AS (
     FROM
         generate_series('2020-07-15', now(), INTERVAL '1 day') day
     LEFT JOIN (
-        SELECT date(cyberlink."timestamp") AS date,
-        count(cyberlink."timestamp") FILTER (WHERE cyberlink.object_from = 'QmbdH2WBamyKLPE5zu4mJ9v49qvY8BFfoumoVPMR5V4Rvx') AS tweets
-        FROM cyberlink
-        WHERE cyberlink."timestamp" > '2020-07-14'
-        GROUP BY (date(cyberlink."timestamp"))
-        ORDER BY (date(cyberlink."timestamp"))
+        SELECT
+            date(cyberlink."timestamp") AS date,
+            count(cyberlink."timestamp") FILTER (WHERE cyberlink.object_from = 'QmbdH2WBamyKLPE5zu4mJ9v49qvY8BFfoumoVPMR5V4Rvx') AS tweets
+            FROM cyberlink
+            WHERE cyberlink."timestamp" > '2020-07-14'
+            GROUP BY (date(cyberlink."timestamp"))
+            ORDER BY (date(cyberlink."timestamp"))
     ) tweets
     ON (
         day::date = tweets."date"
@@ -25,11 +26,12 @@ CREATE VIEW tweets_total AS (
     FROM
         generate_series('2020-07-15', now(), INTERVAL '1 day') day
     LEFT JOIN (
-        SELECT tweets_per_day.date,
-        tweets_per_day.tweets,
-        sum(tweets_per_day.tweets) OVER (ORDER BY tweets_per_day.date) AS total
-        FROM tweets_per_day
-        ORDER BY tweets_per_day.date
+        SELECT
+            tweets_per_day.date,
+            tweets_per_day.tweets,
+            sum(tweets_per_day.tweets) OVER (ORDER BY tweets_per_day.date) AS total
+            FROM tweets_per_day
+            ORDER BY tweets_per_day.date
     ) tw
     ON (
         day::date = tw.date
@@ -42,5 +44,3 @@ CREATE VIEW time_series AS (
     FROM
     generate_series('2020-04-04', now(), INTERVAL '1 day') day
 );
-
-
