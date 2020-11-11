e4caf80918a29065b4e185a6c88b660e51f80ca7
Ales Puchilo <ales.puchilo@gmail.com>
2020-11-11 20:07:41 +0300

    remove unique from object

--- schema/object.sql
+++ schema/object.sql
@@ -1,7 +1,7 @@
 CREATE TABLE object
 (
     id SERIAL PRIMARY KEY,
-    object VARCHAR(256) NOT NULL UNIQUE,
+    object VARCHAR(256) NOT NULL,
     subject CHAR(44) NOT NULL,
     timestamp timestamp without time zone NOT NULL,
     height integer NOT NULL REFERENCES block(height),
