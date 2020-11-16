be12c331904b7f419d59a28579b544d820816d65
Ales Puchilo <ales.puchilo@gmail.com>
2020-11-16 15:04:10 +0300

    Ad cohorts materialized view

---
+++ schema/cohorts.sql
@@ -0,0 +1,96 @@
+CREATE MATERIALIZED VIEW tx_order AS (
+    SELECT *
+    FROM (
+        SELECT
+          transaction.subject,
+          row_number() OVER (
+            PARTITION BY transaction.subject
+            ORDER BY
+              transaction."timestamp"
+          ) AS ordering,
+          transaction.txhash,
+          transaction."timestamp",
+          tp.type
+        FROM
+          (
+            transaction
+            LEFT JOIN (
+              SELECT
+                message.subject,
+                message.txhash,
+                message.type
+              FROM
+                message
+            ) tp ON (tp.txhash = transaction.txhash)
+          )
+        ORDER BY
+          transaction."timestamp"
+    ) tmp
+    WHERE tmp.ordering = 1 OR tmp.ordering = 2
+);
+
+CREATE UNIQUE INDEX ON tx_order (
+    object,
+    ordering
+);
+
+CREATE VIEW cohorts AS (
+    SELECT
+        final_result.subject,
+        date(final_result.register) AS register,
+        date(final_result.first_act) AS first_act,
+        reg."type" AS register_act_type,
+        scnd."type" AS first_act_type,
+        date(date_trunc('month', reg."timestamp")) AS register_act_month,
+        CASE
+            WHEN final_result.first_act IS NULL THEN -1
+            ELSE TRUNC(DATE_PART('day', scnd."timestamp" - reg."timestamp")/30)
+            END AS diff,
+        CASE
+            WHEN final_result.subject IN (SELECT subject FROM gift_info WHERE ethereum IS NOT NULL) THEN 'ethereum'
+            WHEN final_result.subject IN (SELECT subject FROM gift_info WHERE cosmos IS NOT NULL) THEN 'cosmos'
+            WHEN final_result.subject IN (SELECT subject FROM gift_info WHERE urbit IS NOT NULL) THEN 'urbit'
+            WHEN final_result.subject IN (SELECT subject FROM gift_info WHERE euler4 IS NOT NULL) THEN 'euler4'
+            ELSE 'new'
+            END AS tag
+    FROM
+        crosstab('SELECT subject, ordering, "timestamp" FROM tx_order ORDER BY subject, "timestamp", ordering') AS final_result(subject char(44), register timestamp, first_act timestamp)
+    LEFT JOIN (
+        SELECT
+            subject,
+            "timestamp",
+            "type"
+        FROM tx_order
+        WHERE
+            ordering = 1
+    ) reg
+    ON (
+        final_result.subject = reg.subject
+    )
+    LEFT JOIN (
+        SELECT
+            subject,
+            "timestamp",
+            "type"
+        FROM tx_order
+        WHERE
+            ordering = 2
+    ) scnd
+    ON (
+        final_result.subject = scnd.subject
+    )
+);
+
+CREATE OR REPLACE FUNCTION refresh_tx_order()
+RETURNS TRIGGER LANGUAGE plpgsql
+AS $$
+BEGIN
+REFRESH MATERIALIZED VIEW CONCURRENTLY tx_order;
+RETURN NULL;
+END $$;
+
+CREATE TRIGGER refresh_tx_order
+AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
+ON transaction
+FOR EACH STATEMENT
+EXECUTE PROCEDURE refresh_tx_order();
--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -49,9 +49,11 @@ docker exec -ti cyberindex_postgres psql -f /root/schema/twitter.sql -d $POSTGRE
 docker exec -ti cyberindex_postgres psql -f /root/schema/cyberlinks.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
 docker exec -ti cyberindex_postgres psql -f /root/schema/gift_info.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
 
-docker exec -ti cyberindex_postgres psql -c "\copy gift_info FROM /root/schema/gift_info.csv with csv HEADER" $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
+docker exec -ti cyberindex_postgres psql -c "\copy gift_info FROM /root/schema/gift_info.csv with csv HEADER" -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
 
 docker exec -ti cyberindex_postgres psql -f /root/schema/accounts_analytics.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
+
+docker exec -ti cyberindex_postgres psql -f /root/schema/cohorts.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
 
 docker-compose up -d additional-crawlers 
 
