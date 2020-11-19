eba2c8e0749e76e8e1318cd503df4f0561f2b864
Ales Puchilo <ales.puchilo@gmail.com>
2020-11-19 16:58:27 +0300

    refactor cyberindex launch

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -35,27 +35,8 @@ docker exec -ti cyberindex_postgres psql -f /root/schema/pre_commit.sql -d $POST
 docker exec -ti cyberindex_postgres psql -f /root/schema/block.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
 docker exec -ti cyberindex_postgres psql -f /root/schema/transaction.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
 docker exec -ti cyberindex_postgres psql -f /root/schema/cyberlink.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-
 docker exec -ti cyberindex_postgres psql -f /root/schema/relevance.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
 docker exec -ti cyberindex_postgres psql -f /root/schema/message.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-
-docker exec -ti cyberindex_postgres psql -f /root/schema/karma.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-docker exec -ti cyberindex_postgres psql -f /root/schema/rewards.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
 docker exec -ti cyberindex_postgres psql -f /root/schema/object.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-
-docker exec -ti cyberindex_postgres psql -f /root/schema/acc_by_activity.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-docker exec -ti cyberindex_postgres psql -f /root/schema/twitter.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-docker exec -ti cyberindex_postgres psql -f /root/schema/cyberlinks.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-
-docker exec -ti cyberindex_postgres psql -f /root/schema/gift_info.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-docker exec -ti cyberindex_postgres psql -c "\copy gift_info FROM /root/schema/gift_info.csv with csv HEADER" -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-
-docker exec -ti cyberindex_postgres psql -f /root/schema/pre_commit_view.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-docker exec -ti cyberindex_postgres psql -c "\copy old_pre_commits FROM /root/schema/old_pre_commits.csv with csv HEADER" -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-
-docker exec -ti cyberindex_postgres psql -f /root/schema/accounts_analytics.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-docker exec -ti cyberindex_postgres psql -f /root/schema/cohorts.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-
-docker-compose up -d additional-crawlers 
 
 docker run -d --name cyberindex --network="host" cyberindex:latest
