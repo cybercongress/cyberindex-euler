c39618d8a9e7d4722499edd8b1eec87c83c2713a
Ales Puchilo <ales.puchilo@gmail.com>
2020-11-21 20:28:58 +0300

    add recipients to analytics data

--- schema/accounts_analytics.sql
+++ schema/accounts_analytics.sql
@@ -274,20 +274,51 @@ CREATE VIEW new_per_day AS (
             count(first_tx) as new
         FROM (
             SELECT
-                date(min("timestamp")) as first_tx,
+                min(first_tx) AS first_tx,
                 subject
-            FROM
-                transaction
+            FROM (
+                SELECT
+                    date(min("timestamp")) as first_tx,
+                    subject
+                FROM
+                    transaction
+                WHERE
+                    code = '0'
+                GROUP BY
+                    subject
+                UNION
+                SELECT
+                    date(min("timestamp")) AS first_tx,
+                    BTRIM(recipient, '\"') :: char(44) AS subject
+                FROM (
+                    SELECT
+                        recipient,
+                        "timestamp",
+                        row_number() OVER (
+                            PARTITION BY recipient
+                            ORDER BY "timestamp"
+                        ) AS ordering
+                    FROM (
+                        SELECT
+                            text(value :: json -> 'to_address') AS recipient,
+                            "timestamp"
+                        FROM
+                            message
+                        WHERE
+                            "type" = 'cosmos-sdk/MsgSend' AND
+                            code = 0
+                    ) temp
+                ) temp
+                GROUP BY subject
+            ) temp
             WHERE
-                code = '0' AND
                 NOT EXISTS (
-                    SELECT  NULL
-                    FROM    gift_info
-                    WHERE   gift_info.subject = transaction.subject
+                        SELECT  NULL
+                        FROM    gift_info
+                        WHERE   gift_info.subject = temp.subject
                 )
-            GROUP BY
-                subject
-            ORDER BY first_tx asc
+            GROUP BY subject
+            ORDER BY first_tx
         ) tmp
         GROUP BY first_tx
     ) tmp
@@ -336,7 +367,30 @@ CREATE VIEW unique_per_day AS (
                 code = '0'
             GROUP BY
                 subject
-            ORDER BY first_tx asc
+            UNION
+            SELECT
+                date(min("timestamp")) AS first_tx,
+                BTRIM(recipient, '\"') :: char(44) AS subject
+            FROM (
+                SELECT
+                    recipient,
+                    "timestamp",
+                    row_number() OVER (
+                        PARTITION BY recipient
+                        ORDER BY "timestamp"
+                    ) AS ordering
+                FROM (
+                    SELECT
+                        text(value :: json -> 'to_address') AS recipient,
+                        "timestamp"
+                    FROM
+                        message
+                    WHERE
+                        "type" = 'cosmos-sdk/MsgSend' AND
+                        code = 0
+                ) temp
+            ) temp
+            GROUP BY subject
         ) tmp
         GROUP BY first_tx
     ) tmp
